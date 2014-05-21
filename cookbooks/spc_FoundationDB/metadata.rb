8d252395b796b0fc555c1344a001123a9d761048
Matt Baldwin <baldwinmathew@gmail.com>
2014-05-21 16:41:07 -0700

    just a simple recipe to test.
    Change-Id: Ic879fb920bf11c555355b5e317b13a2c8d1528ed

---
+++ cookbooks/spc_FoundationDB/metadata.rb
@@ -0,0 +1,9 @@
+name             'spc_FoundationDB'
+maintainer       'StackPointCloud, Inc.'
+maintainer_email 'dev@stackpointcloud.com'
+license          'All rights reserved'
+description      'Installs/Configures FoundationDB'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe "spc_FoundationDB::default","logs output"
---
+++ cookbooks/spc_FoundationDB/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: spc_FoundationDB
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World! Boom!"
