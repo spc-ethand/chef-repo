a470a73bf5963d7287c8c9bba47329ab2c764cb5
Matt Baldwin <baldwinmathew@gmail.com>
2014-05-21 17:33:17 -0700

    just playing
    Change-Id: Idc2c3d9cb34ba9e7e05d4f3627c5c1474bea06eb

--- cookbooks/spc_FoundationDB/recipes/default.rb
+++ cookbooks/spc_FoundationDB/recipes/default.rb
@@ -8,3 +8,16 @@
 #
 
 log "Hello World! Boom!"
+
+package 'httpd' do
+	action :install
+end
+
+service 'httpd' do
+	action [ :enable, :start ]
+end
+
+cookbook_file '/var/www/html/index.html' do
+	source 'index.html'
+	mode '0644'
+end
