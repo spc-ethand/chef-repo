64c055c4765d00529914a93a703d911683ae4cd5
Matt Baldwin <baldwinmathew@gmail.com>
2014-05-24 09:58:05 -0700

    testing through FDB install

--- cookbooks/spc_FoundationDB/recipes/default.rb
+++ cookbooks/spc_FoundationDB/recipes/default.rb
@@ -7,17 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
-log "Installing HTTPd"
+#log "Installing HTTPd"
 
-package 'httpd' do
-	action :install
-end
+#package 'httpd' do
+#	action :install
+#end
 
-service 'httpd' do
-	action [ :enable, :start ]
-end
+#service 'httpd' do
+#	action [ :enable, :start ]
+#end
 
-cookbook_file '/var/www/html/index.html' do
-	source 'index.html'
-	mode '0644'
-end
+#cookbook_file '/var/www/html/index.html' do
+#	source 'index.html'
+#	mode '0644'
+#end
